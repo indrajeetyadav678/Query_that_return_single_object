d3fd2b78c3fbc47f96d622bc5949905328188532
Indrajeet <yindrajeet678@gmail.com>
2024-04-09 20:32:14 +0530

    second commit

--- project/app/models.py
+++ project/app/models.py
@@ -11,4 +11,14 @@ class Studentmodel(models.Model):
         return self.Name
     
 
-
+class Marksheetmodel(models.Model):
+    Name=models.CharField(max_length=200)
+    Math=models.IntegerField()
+    Science=models.IntegerField()
+    Social_science=models.IntegerField()
+    Hindi=models.IntegerField()
+    English=models.IntegerField()
+    Total_marks=models.IntegerField()
+    Percentage=models.IntegerField()
+    def __str__(self):
+        return self.Name
--- project/app/views.py
+++ project/app/views.py
@@ -1,20 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Studentmodel
+from .models import Studentmodel,Marksheetmodel
 # Create your views here.
+def home(request):
+    data=Studentmodel.objects.create(
+        Name="Mohan singh",
+        City="Gujrat",
+        Email="gaurav@gmail.com",
+        Contact="7888283663"
+    )
+    print(data)
+    return HttpResponse(data)
+
+# def home1(request):
+#     data=Studentmodel.objects.filter(Name="neeraj").delete()
+#     print(data)
+#     return HttpResponse(data)
+# def home1(request):
+#     data=Studentmodel.objects.first()
+#     print(data)
+#     return HttpResponse(data)
+# def home1(request):
+#     data=Studentmodel.objects.last()
+#     print(data)
+#     return HttpResponse(data)
 # def home(request):
-#     data=Studentmodel.objects.create(
-#         Name="neeraj",
-#         City="bhopal",
-#         Email="indrajeet@gmail.com",
-#         Contact="999776855"
+#     data=Studentmodel.objects.filter(Name="Gaurav").update(
+#         Name="Saurav",
+#         Contact="788828366300000"
 #     )
 #     print(data)
 #     return HttpResponse(data)
 
-def home(request):
-    data=Studentmodel.objects.last().delete()
-    print(data)
-    return HttpResponse(data)
+# def home(request):
+#     data=Studentmodel.objects.filter(Name="Mohan singh").delete()
+#     print(data)
+#     return HttpResponse(data)
 #last()
 #get()
+
+# ========================================MarksheetModel  =================================
+# def mark(request):
+#     data=Marksheetmodel.objects.create(
+#         Name="indrajeet",
+
+#     )
